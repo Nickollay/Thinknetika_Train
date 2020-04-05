283708528a6d508bed8db1c2a243a98cb56bffd1
nickollay <nickollaysokolov@gmail.com>
2020-04-05 21:58:41 +0300

    Add methods about seats and volume to pass, cargo carriages.

--- cargo_carriage.rb
+++ cargo_carriage.rb
@@ -1,5 +1,26 @@
 class CargoCarriage < Carriage
+  atrr_reader :free_volume, :taken_volume
+
+  def initialize(number, volume)
+    super(number)
+    @volume = volume
+    @free_volume = @volume
+    @taken_volume = @volume - @free_volume
+  end
+
   def type
     @type = 'cargo'
+  end
+
+  def take_volume(cargo)
+    validate_free_volume!
+    @free_volume -= cargo
+  end
+
+  private
+
+  def validate_free_volume!
+    raise 'No free volume anymore!' if @free_volume == 0
+    raise 'There are not enough free volume!' if cargo > @free_volume
   end
 end
--- passenger_carriage.rb
+++ passenger_carriage.rb
@@ -1,5 +1,26 @@
+require_relative 'carriage'
 class PassengerCarriage < Carriage
+  attr_reader :free_seats, :taken_seats
+
+  def initialize(number, seats)
+    super(number)
+    @seats = seats
+    @free_seats = @seats
+    @taken_seats = @seats - @free_seats
+  end
+
   def type
     @type = 'pass'
+  end
+
+  def take_seat
+    validate_free_seats!
+    @free_seats -= 1
+  end
+
+  private
+
+  def validate_free_seats!
+    raise 'All seats taken!' if @free_seats.zero?
   end
 end
